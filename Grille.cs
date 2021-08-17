0f55ba5275e6993a0b0b3acf4816fa35ed813d52
69019-03-02 <69019-03-02@STA5329897>
2021-08-17 14:41:16 +0200

    Ajoutez des fichiers projet.

---
+++ Grille.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace SudokuPOO
+{
+    // Classe "Grille". Représente une grille de sudoku. 
+    public class Grille
+    {
+        private List<int>[] _Valeurs;  // Une grille est un tableau de liste. Tableau de 10 listes.
+                                       // La première liste (indice 0) est la liste des cases résolues.
+                                       // La Nième liste est la liste des cases pour laquelle la valeur N est possible.
+
+        public Grille() // Constructeur de grille vide. Dans une grille vide, toutes les valeurs sont possibles partout.
+
+        {
+            _Valeurs = new List<int>[10];
+            _Valeurs[0] = new List<int>();
+            for (int valeur = 1; valeur <= 9; valeur++)
+            {
+                _Valeurs[valeur] = new List<int>();
+                for (int IndiceCase = 0; IndiceCase <= 80; IndiceCase++)
+                { _Valeurs[valeur].Add(IndiceCase); }
+            }
+        }
+        public Grille(List<int> Liste_Valeurs) // Constructeur de grille partielle. Prends en paramètre une liste d'entiers.
+                                               // Si l'entier est entre 1 et 9 : c'est la valeur de cette case.
+                                               // Sinon, la case a une valeur inconnue.
+                                               // Si la case a une valeur inconnue, toutes les valeurs y sont possibles.
+        {
+            _Valeurs = new List<int>[10];
+            for (int v = 0; v <= 9; v++) // Initialisation de chaque liste (sinon elle est de type "null" et on ne peut rien y ajouter)
+            { _Valeurs[v] = new List<int>(); }
+            for (int IndiceCase = 0; IndiceCase <= 80; IndiceCase++)
+            {
+                if (Liste_Valeurs[IndiceCase] > 9 || Liste_Valeurs[IndiceCase] < 1) // Case de valeur inconnue.
+                {
+                    for (int v = 1; v <= 9; v++)
+                    { _Valeurs[v].Add(IndiceCase); } // Toutes les valeurs de 1 à 9 sont possibles, mais la case n'est pas listée comme connue.
+                }
+                else
+                {
+                    _Valeurs[0].Add(IndiceCase); // La case est listée comme connue
+                    _Valeurs[Liste_Valeurs[IndiceCase]].Add(IndiceCase); // La liste des cases ayant cette valeur est mise à jour en y ajoutant l'indice de la case.
+                }
+            }
+        }
+        public static (Grille, Grille) Hypothese(Grille UneGrille) // Fais une hypothèse sur une grille non résolue.
+                                                            // Prend la grille passée en paramètre, renvoie deux grilles-hypothèse complémentaires.
+                                                            // Par exemple la case 5 de la grille en paramètre est inconnue.
+                                                            // Renvoie une grille où cette case vaut 1, et une grille où cette case vaut tout sauf 1.
+                                                            // On s'y prend ainsi : on commence par recopier la grille de départ à l'identique.
+                                                            // Ensuite on prendra une hypothèse sur la plus petite case non résolue.
+        {
+            List<int>[] _ValeursA = new List<int>[10];
+            List<int>[] _ValeursB = new List<int>[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                _ValeursA[i] = new List<int>();
+                _ValeursB[i] = new List<int>();
+                foreach (int UneCase in UneGrille._Valeurs[i])
+                {
+                    _ValeursA[i].Add(UneCase);
+                    _ValeursB[i].Add(UneCase);
+                }
+            }
+            Grille GrilleA = new Grille();
+            Grille GrilleB = new Grille();
+            GrilleA._Valeurs = _ValeursA;
+            GrilleB._Valeurs = _ValeursB;
+            // Maintenant on cherche la première case non résolue
+            int Indice = 0;
+            while (UneGrille._Valeurs[0].Contains(Indice))
+            { Indice++; }
+            // Cette case est résolue dans A, avec sa plus petite valeur possible
+            int valeurA = 1;
+            while (!UneGrille._Valeurs[valeurA].Contains(Indice))
+            { valeurA++; }
+            // la valeur valeurA est possible pour la case Indice. La case Indice prend la valeurA dans la grilleA.
+            for (int v = 1; v <= 9; v++)
+            {
+                if (v != valeurA)
+                {
+                    GrilleA._Valeurs[v].Remove(Indice);
+                }
+            }
+            // la valeur valeurA est interdite dans la case Indice pour la grille B.
+            GrilleB._Valeurs[valeurA].Remove(Indice);
+            return (GrilleA, GrilleB);
+        }
+
+        public int Valeur(int IndiceCetteCase) // Prend en paramètre l'indice d'une case, renvoie la valeur de cette case si la case est connue.
+                                               // Jette une exception si la valeur de la case est inconnue : on est censés n'appeler "Valeur" que sur des cases connues
+
+        {
+            int val = 0;
+            if (_Valeurs[0].Contains(IndiceCetteCase))
+            {
+                for (int v = 1; v <= 9; v++)
+                {
+                    if (_Valeurs[v].Contains(IndiceCetteCase))
+                    { val = v; }
+                }
+            }
+            return val;
+        }
+        public bool EstPossible() // Indique si la grille est possible.
+                                  // Une grille est possible si TOUTES les cases sont possibles.
+                                  // Une case est possible si AU MOINS une valeur est possible.
+        {
+            bool grille_possible = true;
+            for (int indice = 0; indice <=80; indice++)
+            {
+                bool case_possible = false;
+                for (int val = 1;val <= 9; val++)
+                {
+                    case_possible = case_possible || _Valeurs[val].Contains(indice);
+                }
+                grille_possible = grille_possible && case_possible;
+            }
+            return grille_possible;
+        }
+        public bool EstResolue()    // Indique si cette grille est résolue. Une grille est résolue si toutes ses cases sont résolues.
+                                    // Quand toutes les cases sont résolues, la liste des cases résolues comporte toutes les 81 cases.
+        {
+            return _Valeurs[0].Count == 81;
+        }
+        public List<int> Ligne(int IndiceCetteCase) // Prend en paramètre l'indice d'une case.
+                                                    // Renvoie la liste des indices des cases sur la même ligne,
+                                                    // Sauf la case donnée au départ.
+        {
+            List<int> ListeLigne = new List<int>();
+            int IndiceLigne = IndiceCetteCase / 9;
+            for (int indice = IndiceLigne * 9; indice <= IndiceLigne * 9 + 8; indice++) // Parcours de la ligne de cette case
+            {
+                if (indice != IndiceCetteCase)
+                {
+                    ListeLigne.Add(indice);
+                }
+            }
+            return ListeLigne;
+        }
+        public List<int> Colonne(int IndiceCetteCase) // Prend en paramètre l'indice d'une case. 
+                                                      // Renvoie la liste des indices des cases sur la même colonne,
+                                                      // Sauf la case donnée au départ
+        {
+            List<int> ListeColonne = new List<int>();
+            int IndiceColonne = IndiceCetteCase % 9; // Numéro de la colonne, de 0 à 8.
+            for (int indice = 0; indice <= 8; indice++)
+            {
+                if (IndiceColonne + (indice * 9) != IndiceCetteCase)
+                {
+                    ListeColonne.Add(IndiceColonne + (indice * 9));
+                }
+            }
+            return ListeColonne;
+        }
+        public List<int> Bloc(int IndiceCetteCase) // Prend en paramètre l'indice d'une case.
+                                                   // Renvoie la liste des indices des cases dans le même bloc,
+                                                   // Sauf la case donnée au départ
+        {
+            List<int> ListeBloc = new List<int>();
+            static int BlocIndice(int i)   // On définit une fonction qui renvoie l'indice du bloc (de 0 à 8) en fonction de l'indice de la case.
+            { return (3 * (i / 27)) + ((i / 3) % 3); }
+            for (int indice = 0; indice <= 80; indice++)   // On parcours toute la grille pour trouver les cases ayant le même indice de bloc que la case de départ.
+            {
+                if (BlocIndice(indice) == BlocIndice(IndiceCetteCase) && indice != IndiceCetteCase)
+                {
+                    ListeBloc.Add(indice);
+                }
+            }
+            return ListeBloc;
+        }
+
+        public List<int> Voisinage(int IndiceCetteCase) // Le voisinage, c'est la Ligne, la Colonne, ET le Bloc.
+        {
+            List<int> Voisins = new List<int>();
+            Voisins.AddRange(Ligne(IndiceCetteCase));
+            Voisins.AddRange(Colonne(IndiceCetteCase));
+            Voisins.AddRange(Bloc(IndiceCetteCase));
+            return Voisins;
+        }
+
+        public List<int> Liste_Valeurs() // Permet d'obtenir la liste des valeurs connues. Les valeurs inconnues sont remplacées par 0.
+        {
+            List<int> ListeValeurs = new List<int>();
+            for (int indice = 0; indice <= 80; indice++)
+            {
+                int valeur_de_case = 0;
+                if (_Valeurs[0].Contains(indice))
+                {
+                    for (int v = 1; v <= 9; v++)
+                    {
+                        if (_Valeurs[v].Contains(indice))
+                        { valeur_de_case = v; }
+                    }
+                }
+                ListeValeurs.Add(valeur_de_case);
+            }
+            return ListeValeurs;
+        }
+        // Règles du sudoku
+        // 1 - Une valeur ne peut pas se trouver plus d'une fois sur une ligne, une colonne ou un bloc.
+        // 2 - Une valeur doit se trouver une fois sur une ligne, une colonne ou un bloc.
+
+        // Fonction d'application des deux règles :
+        // - Une case connue rend impossible sa valeur sur sa ligne, sa colonne et son bloc.
+        // - Une case inconnue n'ayant qu'une valeur possible prend cette valeur.
+        // Paramètre : aucun (s'applique sur une grille, c'est une méthode)
+        // Effet : Prend chaque case connue, applique ses contraintes au voisinage.
+        // Renvoie : un booléen "possible"
+        // "possible" indique si la grille est pour l'instant possible à résoudre (pas de contradictions détectées), 
+        // On regroupe les deux règles. Algorithme :
+        // On parcourt toutes les cases :
+        // - Si la case est connue elle empêche sa valeur d'être présente dans le voisinage,
+        // - Si la case est inconnue, elle devient connue si une valeur est impossible dans tout son voisinage.
+        // - On a "possible" tant qu'au moins une valeur est possible pour chaque case
+        // On continue la boucle tant qu'on a du "progres", c'est à dire qu'on a réussi à supprimer des valeurs possibles.
+        
+        public bool Regles()
+        {
+            bool possible = true;
+            bool progres;
+            // Fonction d'ajout de case résolue.
+            // Permet d'ajouter une case à la liste des cases résolues.
+            // Vérifie que la case n'est pas déjà dans la liste.
+            // Renvoie "true" si l'ajout a été effectif.
+            // Renvoie "false" si la case était déjà présente.
+            bool ajout_resolue(int IndiceCase)
+            {
+                bool deja_resolue = _Valeurs[0].Contains(IndiceCase);
+                if (deja_resolue)
+                {
+                    return false;
+                }
+                else
+                {
+                    _Valeurs[0].Add(IndiceCase);
+                    return true;
+                }
+            }
+            do
+            {
+                progres = false;
+                for (int CetteCase = 0; CetteCase <= 80; CetteCase++) // On parcourt les cases connues.
+                {
+                    if (_Valeurs[0].Contains(CetteCase)) // CETTE CASE EST CONNUE
+                    {
+                        // Pour chaque case connue : on prend sa valeur.
+                        int ValeurCetteCase = Valeur(CetteCase);
+                        // On interdit cette valeur dans le voisinage : Colonne, ligne et bloc.
+
+                        if (ValeurCetteCase != 0)
+                        {
+                            foreach (int AutreCase in Voisinage(CetteCase))
+                            {
+                                progres = _Valeurs[ValeurCetteCase].Remove(AutreCase) || progres;
+                            }
+                        }
+                    }
+                    else // CETTE CASE EST INCONNUE
+                    {
+                        // CetteCase = Case inconnue. On parcourt les valeurs.
+                        for (int val = 1; val <= 9; val++)
+                        {
+                            int compte_valeur_impossible = 0;
+                            foreach (int AutreCase in Ligne(CetteCase)) // On parcourt la ligne
+                            {
+                                if (!_Valeurs[val].Contains(AutreCase)) // AutreCase ne peut pas prendre la valeur "val"
+                                {
+                                    compte_valeur_impossible++;
+                                }
+                            }
+                            if (compte_valeur_impossible == 8) // La valeur "val" est impossible dans 8 cases de la ligne de CetteCase.
+                                                               // Donc la case CetteCase prend la valeur "val".
+                            {
+                                for (int v = 1; v <= 9; v++)
+                               {
+                                    if (v != val)
+                                    {
+                                        _Valeurs[v].Remove(CetteCase);
+                                    }
+                                }
+                                progres = ajout_resolue(CetteCase) || progres; // CetteCase est maintenant connue.
+                            }
+                            compte_valeur_impossible = 0;
+                            foreach (int AutreCase in Colonne(CetteCase)) // On parcourt la colonne
+                            {
+                                if (!_Valeurs[val].Contains(AutreCase)) // AutreCase ne peut pas prendre la valeur "val"
+                                {
+                                    compte_valeur_impossible++;
+                                }
+                            }
+                            if (compte_valeur_impossible == 8) // La valeur "val" est impossible dans 8 cases de la Colonne de CetteCase.
+                                                               // Donc la case CetteCase prend la valeur "val".
+                            {
+                                for (int v = 1; v <= 9; v++)
+                                {
+                                    if (v != val)
+                                    {
+                                        _Valeurs[v].Remove(CetteCase);
+                                    }
+                                }
+                                progres = ajout_resolue(CetteCase) || progres; // CetteCase est maintenant connue.
+                            }
+                            compte_valeur_impossible = 0;
+                            foreach (int AutreCase in Bloc(CetteCase)) // On parcourt le Bloc
+                            {
+                                if (!_Valeurs[val].Contains(AutreCase)) // AutreCase ne peut pas prendre la valeur "val"
+                                {
+                                    compte_valeur_impossible++;
+                                }
+                            }
+                            if (compte_valeur_impossible == 8) // La valeur "val" est impossible dans 8 cases du Bloc de CetteCase.
+                                                               // Donc la case CetteCase prend la valeur "val".
+                            {
+                                for (int v = 1; v <= 9; v++)
+                                {
+                                    if (v != val)
+                                    {
+                                         _Valeurs[v].Remove(CetteCase);
+                                    }
+                                }
+                                progres = ajout_resolue(CetteCase) || progres;  // CetteCase est maintenant connue.
+                            }
+                        }
+                    }
+                    // Après avoir appliqué les nouvelles contraintes, on passe les cases en "résolues" si on peut les résoudre.
+                    // Case résolue : case pour laquelle il n'y a plus qu'une seule valeur qui est possible.
+                    int compte_val_CetteCase = 0; // Nombre de valeurs possibles pour CetteCase
+                    for (int val = 1; val <= 9; val++)
+                    {
+                        if (_Valeurs[val].Contains(CetteCase)) // CetteCase peut prendre la valeur "val"
+                        {
+                            compte_val_CetteCase++;
+                        }
+                    }
+                    if (compte_val_CetteCase == 0) // Grille impossible : CetteCase ne peut prendre aucune valeur
+                    { 
+                        possible = false;
+                        progres = false;
+                        break;
+                    }
+                    if (compte_val_CetteCase == 1) // La case CetteCase fait son entrée dans la liste des cases résolues
+                    {
+                        progres = ajout_resolue(CetteCase) || progres;
+                    }
+                }
+            } while (progres);
+            return possible;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuPOO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextBox[] TextBoxes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.TextBoxes = GenerateTextBoxes();
+        }
+        public TextBox[] GenerateTextBoxes()
+        {
+            TextBox[] TextBoxes = new TextBox[81];
+            for (int i = 0; i <= 80; i++)
+            {
+                TextBox TBox = new TextBox();
+                TBox.Margin = new Thickness(5 + (25 * (i % 9)), 5 + (25 * (i / 9)), 0, 0);
+                TBox.Text = i.ToString();
+                TBox.Width = 20;
+                TBox.Height = 20;
+                TBox.VerticalAlignment = VerticalAlignment.Top;
+                TBox.HorizontalAlignment = HorizontalAlignment.Left;
+                _ = MaGrille.Children.Add(TBox);
+                TextBoxes[i] = TBox;
+            }
+            return TextBoxes;
+        }
+        public void DrawLines()
+        {
+
+        }
+
+        private void Resoudre(object sender, RoutedEventArgs e)
+        {
+            // Liste d'entiers pour passer les valeurs 
+            List<int> Valeurs = new List<int>();
+            // Boucle sur les TextBoxes pour remplir la liste
+            for (int i = 0; i <= 80; i++)
+            {
+                // On passe par une variable intermédiaire
+                int valeur_i = 0;
+                bool parse0 = int.TryParse(TextBoxes[i].Text, out valeur_i);
+                Valeurs.Add(valeur_i);
+            }
+
+            Grille Sudoku = new Grille(Valeurs);
+            bool SudokuPossible = Sudoku.Regles();
+            if (!SudokuPossible)
+            {
+                var impossible = new info_popup();
+                impossible.Owner = this;
+                impossible.ShowDialog();
+            }
+            
+            if (SudokuPossible && Sudoku.EstResolue())
+            {
+                Afficher(Sudoku);
+                var termine = new info_popup();
+                termine.Owner = this;
+                termine.Title = "Terminé";
+                termine.Bouton_ok.Content = "OK";
+                termine.infotext.Text = "Grille résolue :)";
+                termine.ShowDialog();
+
+            }
+
+
+            if (SudokuPossible && ! Sudoku.EstResolue()) // La grille n'est pas résolue, on va devoir faire des hypothèses.
+            {
+                int max_grilles = 55; // On limite le nombre de grilles générées pour ne pas planter.
+                                      // 55 est une valeur obtenue par tâtonnement : dans le pire des cas (grille vide), en limitant à 55 grilles générées,
+                                      // On arrive malgré tout à deux solutions. On peut donc déterminer dans n'importe quel cas si la grille a une ou plusieurs solutions.
+                int compteur_grilles = 0;
+                List<Grille> ListeGrillesResolues = new List<Grille>(); // On va faire la liste des solutions pour les compter.
+                List<Grille> ListeGrillesAResoudre = new List<Grille>(); // On va stocker les grilles à résoudre.
+                ListeGrillesAResoudre.Add(Sudoku);
+                do
+                {
+                    Grille GrilleAResoudre = ListeGrillesAResoudre.Last();
+                    ListeGrillesAResoudre.Remove(GrilleAResoudre);
+                    (Grille GrilleA, Grille GrilleB) = Grille.Hypothese(GrilleAResoudre);
+                    if (GrilleA.Regles())
+                    {
+                        if (GrilleA.EstResolue())
+                        {
+                            ListeGrillesResolues.Add(GrilleA);
+                            compteur_grilles++;
+                        }
+                        else // La GrilleA est possible mais pas résolue
+                        {
+                            if (GrilleB.Regles())
+                            {
+                                if (GrilleB.EstResolue()) // La Grille B est résolue
+                                {
+                                    ListeGrillesResolues.Add(GrilleB);
+                                    compteur_grilles++;
+                                }
+                                else // Aucune des grilles n'est résolue. On reste sur la branche A, plus déterministe, en plaçant la GrilleA en dernière dans la liste.
+                                     // ça nous permettra de récupérer la GrilleA au prochain tour.
+                                     // Même dans le pire cas (grille vide), procéder ainsi nous assure d'avoir au moins une case résolue par cycle d'hypothèses.
+                                {
+                                    ListeGrillesAResoudre.Add(GrilleB);
+                                    ListeGrillesAResoudre.Add(GrilleA); 
+                                    compteur_grilles++;
+                                }
+                            }
+                        }
+                    }
+                } while (compteur_grilles < max_grilles && ListeGrillesAResoudre.Count > 0);
+                if (ListeGrillesResolues.Count == 0)
+                {
+                    var impossible = new info_popup(); // Par défaut info_popup affiche le message "Grille Impossible"
+                    impossible.Owner = this;
+                    impossible.ShowDialog();
+                }
+                if (ListeGrillesResolues.Count == 1)
+                {
+                    Afficher(ListeGrillesResolues.Last());
+                    var termine = new info_popup();
+                    termine.Owner = this;
+                    termine.Title = "Terminé";
+                    termine.Bouton_ok.Content = "OK";
+                    termine.infotext.Text = "Grille résolue :)";
+                    termine.ShowDialog();
+
+                }
+                if (ListeGrillesResolues.Count > 1)
+                {
+                    Afficher(ListeGrillesResolues.Last());
+                    var Plusieurs_Grilles = new info_popup();
+                    Plusieurs_Grilles.Owner = this;
+                    Plusieurs_Grilles.Title = "Plusieurs possibilités";
+                    Plusieurs_Grilles.Bouton_ok.Content = "OK";
+                    Plusieurs_Grilles.infotext.Text = "Il existe plusieurs solutions.\n En voici une.";
+                    Plusieurs_Grilles.ShowDialog();
+                }
+            }
+        }
+        private void Afficher(Grille UneGrille)
+        {
+            List<int> ListeValeurs = UneGrille.Liste_Valeurs();
+            for (int indice = 0; indice <= 80; indice++)
+            {
+                if (ListeValeurs[indice] != 0)
+                { TextBoxes[indice].Text = ListeValeurs[indice].ToString(); }
+            }
+        }
+        private void Effacer(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBox TB in TextBoxes)
+            { TB.Text = ""; }
+        }
+    }
+}
